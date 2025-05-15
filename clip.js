b5bf2c35cda9413139067d7eda21ab5642ec1ae5
Maxwell Martin <maxmart97@gmail.com>
2025-05-15 14:36:29 -0400

    convert to bookmarklet format

--- clip.js
+++ clip.js
@@ -1,44 +1,52 @@
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+javascript: (async () => {
+  
+	const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
-async function clipCoupons() {
-	var btnShowMore = document.querySelector("#show-more");
-	
-	// Click the "Show More" button element until all coupons appear on page
-	while (btnShowMore) {
-		btnShowMore.click();
-		await sleep(15000);
-		btnShowMore = document.querySelector("#show-more");
+	async function clipCoupons() {
+		var btnShowMore = document.querySelector("#show-more");
+		
+		/* Click the "Show More" button element until all coupons appear on page */
+		while (btnShowMore) {
+			console.log(`Loading all coupons onto the page...`);
+			btnShowMore.click();
+			await sleep(3000);
+			btnShowMore = document.querySelector("#show-more");
+		}
+		
+		const couponsUnordList = document.querySelector("#panel-browse > div:nth-child(2) > ul");
+		
+		if (!couponsUnordList) {
+			console.error("Unordered list not found");
+			return;
+		}
+
+		const listItems = couponsUnordList.querySelectorAll("li");
+		var index = 0;
+
+		for (const listItem of listItems) {
+			var rndInt = Math.floor(Math.random() * (5 - 3 + 1)) + 3;
+			const buttons = listItem.querySelectorAll("button");
+			
+			buttons.forEach((button) => {
+				/* Look for a <div> inside the button that exactly contains "Clip Coupon" */
+				const hasClipCouponDiv = Array.from(button.querySelectorAll("div")).some(div => div.textContent.trim() === "Clip Coupon");
+
+				if (hasClipCouponDiv) {
+					console.log(`Clicking 'Clip Coupon' button in list item ${index + 1}`);
+					button.click();
+				}		
+			});
+			
+			index++;
+			await sleep(rndInt * 250);
+		}
 	}
-	
-	const couponsUnordList = document.querySelector("#panel-browse > div:nth-child(2) > ul");
-	
-	if (!couponsUnordList) {
-		console.error("Unordered list not found");
-		return;
+  
+	async function main() {
+		await clipCoupons();
+		console.log ("All coupons have been clipped.");
 	}
 
-	const listItems = couponsUnordList.querySelectorAll("li");
-
-	listItems.forEach((li, index) => {
-		const buttons = li.querySelectorAll("button");
-
-		buttons.forEach((button) => {
-			// Look for a <div> inside the button that exactly contains "Clip Coupon"
-			const hasClipCouponDiv = Array.from(button.querySelectorAll("div")).some(div => div.textContent.trim() === "Clip Coupon);
-
-			if (hasClipCouponDiv) {
-				console.log(`Clicking 'Clip Coupon' button in list item ${index + 1}`);
-				button.click();
-			}		
-		});
-	
-		await sleep(15000);
-	});
-}
-
-async function main() {
-	await clipCoupons();
-	console.log ("All coupons have been clipped.");
-}
-
-main();
+	main();
+  
+})();
